049a19cdc4e1166ccf9ac145f1a88b29327ecb7a
Aaron Baldwin <baldwina@gmail.com>
2010-04-16 15:51:24 -0600

    fix printable pdf format check

--- lib/composable_builders/components/printable.rb
+++ lib/composable_builders/components/printable.rb
@@ -1,8 +1,6 @@
 module ComposableBuilders
   module Components
     module Printable
-      TEMPLATE_FORMAT = :pdf
-  
       def self.included( base )
         base.extend(ClassMethods)
         base.class_eval do
@@ -10,12 +8,12 @@ module ComposableBuilders
           self.define_methods
         end
       end
-  
+      
       module ClassMethods
         def define_methods
           %w(date_select select text_field).each do |method|
             define_method(method.to_sym) do |*args|
-              if @template.template_format == TEMPLATE_FORMAT
+              if @template.request.format.pdf?
                 send("#{method}_for_pdf", *args)
               else
                 super(*args)
@@ -24,25 +22,25 @@ module ComposableBuilders
           end
         end
       end
-  
+      
       module InstanceMethods
         def select_for_pdf(method, choices, options = {}, html_options = {})
           return blank_line if options[:blank_line]
-          @template.content_tag(:ul, (choices.map { |c| @template.content_tag(:li, Array(c).first) }), :class =>'list')
+          @template.content_tag(:ul, (choices.map { |c| @template.content_tag(:li, Array(c).first) } * ''), :class =>'list')
         end
-
+        
         def date_select_for_pdf(*args)
           blank_line
         end
-
+        
         def text_field_for_pdf(*args)
           blank_line
         end
-
+        
         #######
         private
         #######
-
+        
         def blank_line
           @template.content_tag(:span, ('_' * 40), :class => 'line')
         end
